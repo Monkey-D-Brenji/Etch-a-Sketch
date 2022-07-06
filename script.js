fb8e007b9291a4635d63a0b7763c4b97f30c511d
GodUsopp <an.brendancode@gmail.com>
2022-07-05 22:03:03 -0700

    Create Etch-a-Sketch
    Made basic HTML skeleton for Etch-a-Sketch. Created several functions
    with some DOM manipulation to create grid box. Added event listner
    with some CSS styles to change the color.

---
+++ script.js
@@ -0,0 +1,41 @@
+const container = document.getElementById("container");
+const rows = document.getElementsByClassName("gridrow");
+const btn = document.querySelector("button");
+
+// Create a default grid sized 16x16
+
+// Functions to make Grid
+// Row Function
+function gridRow(num) {
+  for (i = 0; i < num; i++) {
+    const row = document.createElement("div");
+    row.classList.add("gridrow");
+    container.appendChild(row);
+  }
+}
+
+// Column Function
+function gridColumn(num) {
+  for (i = 0; i < num; i++) {
+    for (j = 0; j < rows.length; j++) {
+      const column = document.createElement("div");
+      column.classList.add("cell");
+      rows[j].appendChild(column);
+    }
+  }
+}
+
+function makeGrid(row, cell) {
+  gridRow(row);
+  gridColumn(cell);
+}
+
+makeGrid(16, 16);
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach(function (element) {
+  element.addEventListener("mouseover", () => {
+    element.classList.add("hover");
+  });
+});
